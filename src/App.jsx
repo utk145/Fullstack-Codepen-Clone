c805e17cbb195ac2f9c7dae0f7aaeeaf277cda09
utk145 <utkarsh20241a0514@grietcollege.com>
2023-10-29 16:27:51 +0530

    initial routing setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
 
 
 function App() {
- 
+
 
   return (
-    <>
-    <h1 className="underline text-3xl font-bold text-red-500">Basic set up done</h1>
-    </>
+    <div className="w-screen h-screen flex items-start justify-start overflow-hidden">
+      <Routes>
+        <Route path="/home/*" element={<Home />} />
+
+        
+
+        {/* If no route path matches*/}
+        <Route path="*" element={<Navigate to="/home"/>}/>
+      </Routes>
+    </div>
   )
 }
 
