47a90a6bd4d889fc0ec25f1889352dbfe2a6545b
utk145 <utkarsh20241a0514@grietcollege.com>
2023-11-04 15:22:06 +0530

    firebase auth setup ✔

---
+++ src/configuration/firebase.config.js
@@ -0,0 +1,25 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
+};
+
+
+// Initialize Firebase
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+
+
+// Auth information
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+
+
+export { auth, app, db };
