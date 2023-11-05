9b0c33676af4bc192aa11abd1a00dbd399103a94
utk145 <utkarsh20241a0514@grietcollege.com>
2023-11-05 14:05:25 +0530

    signin with github oauth caliberated and popup working ✔

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,14 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../configuration/firebase.config";
 
 export const signInWithGoogle = async () => {
     await signInWithPopup(auth, new GoogleAuthProvider())
+        .then(userCredential => {
+            window.location.reload();
+        })
+}
+export const signInWithGithub = async () => {
+    await signInWithPopup(auth, new GithubAuthProvider())
         .then(userCredential => {
             window.location.reload();
         })
