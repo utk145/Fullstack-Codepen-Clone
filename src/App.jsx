2827c11e8116b9ecf825b38da05564c51b8a9095
utk145 <utkarsh20241a0514@grietcollege.com>
2023-11-08 06:58:53 +0530

    pushing the data to the firestore ✔

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 import { useEffect } from "react"
 import { Navigate, Route, Routes, useNavigate } from "react-router-dom"
 import Home from "./components/Home"
-import { auth } from "./configuration/firebase.config"
+import { auth, db } from "./configuration/firebase.config"
 
 
 function App() {
@@ -13,10 +14,20 @@ function App() {
     const unsub = auth.onAuthStateChanged(userCred => {
       if (userCred) {
         console.log(userCred?.providerData[0]);
+        // setDoc(doc(db, "users", userCred?.uid), userCred?.providerData[0])
+        addDoc(collection(db, "users"), userCred?.providerData[0])
+          .then(() => {
+            // Here the data will be dispatched to redux store 
+
+          })
       } else {
         navigate("/home/auth", { replace: true });
       }
     })
+
+    // cleaning up the listener event
+    return () => unsub();
+    
   }, [])
 
 
