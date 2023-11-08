9e7d174ae63610a699dfd8d5e8080b2d5f0bda8f
utk145 <utkarsh20241a0514@grietcollege.com>
2023-11-08 10:48:39 +0530

    loader ✔

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { addDoc, collection, doc, setDoc } from "firebase/firestore";
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Navigate, Route, Routes, useNavigate } from "react-router-dom"
 import Home from "./components/Home"
+import Loader from "./components/Loader";
 import { auth, db } from "./configuration/firebase.config"
 
 
 function App() {
 
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+
 
   // Getting info of logged in individual
   useEffect(() => {
@@ -23,25 +26,41 @@ function App() {
       } else {
         navigate("/home/auth", { replace: true });
       }
+      setInterval(() => {
+        setIsLoading(false);
+      }, 2000);
     })
 
     // cleaning up the listener event
     return () => unsub();
-    
+    /*
+      This is where the cleanup is happening. When the component is unmounted or when the dependency array (the second argument to useEffect) changes, React will automatically invoke the cleanup function. In this case, the cleanup function calls unsub(), which unsubscribes from the event listener established with auth.onAuthStateChanged. This ensures that you remove the event listener to prevent memory leaks and unwanted side effects when the component is unmounted or re-rendered.
+
+      So, in summary, the cleanup in your code is achieved by returning a function that unsubscribes from the authentication state change event listener when the component is unmounted or when the dependency array changes (in this case, it's an empty array, so it only runs once on mount and once on unmount).
+    */
   }, [])
 
 
   return (
-    <div className="w-screen h-screen flex items-start justify-start overflow-hidden bg-[#131417] bg-gradient-to-tr from-[#1A1A4E] to-[#292A75]">
-      <Routes>
-        <Route path="/home/*" element={<Home />} />
+    <>
+      {isLoading ? (
+        <div className="flex w-screen h-screen items-center justify-center bg-[#030303] bg-gradient-to-tr from-[#1A1A4E] to-[#040417] overflow-hidden">
+          <Loader />
+        </div>
+      )
+        :
+        <div className="w-screen h-screen flex items-start justify-start overflow-hidden bg-[#131417] bg-gradient-to-tr from-[#1A1A4E] to-[#292A75]">
+          <Routes>
+            <Route path="/home/*" element={<Home />} />
 
 
 
-        {/* If no route path matches*/}
-        <Route path="*" element={<Navigate to="/home" />} />
-      </Routes>
-    </div>
+            {/* If no route path matches*/}
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        </div >
+      }
+    </>
   )
 }
 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+import { DotLoader } from "react-spinners"
+
+const Loader = () => {
+  return <DotLoader color="#4fa94d" size={85}/>
+}
+
+export default Loader
